bdd625b4b0af304fd252623f09193559cf785833
Vinh Lê <lephucvinh184@gmail.com>
2023-05-01 01:09:34 +0700

    TE-2: Update UI Header

--- src/routes/Header.js
+++ src/routes/Header.js
@@ -19,25 +19,21 @@ function Header() {
         </div>
       </div>
 
-      <div className='bg-[#D2ECED] flex h-56 items-start'>
-        <div>
-          <div className='flex justify-start'>
-            <img
-              className='h-56'
-              src='https://raw.githubusercontent.com/tinophan247/Tino-Ecommerce/2537a9043cacddbf03b0f4b35773742ddf111f62/public/images/logo.png'
-            />{' '}
+      <div className='bg-[#D2ECED] h-56 flex justify-center '>
+        <div className='w-1600 items-start justify-between flex'>
+        <img className='h-56' src='./images/logo.png' alt='not-found' />
+        
+
+          
+            <div className='flex mt-20 ml-16'>
+              <input className='w-700 h-14'></input>
+              <button className=' bg-blue-300 h-14 w-14 hover:text-blue-900 justify-center'>
+                <SearchIcon />
+              </button>
+            
           </div>
-        </div>
-        <div>
-          <div className='search-box flex justify-center mt-20 ml-16'>
-            <input className='search-box_input type-text w-100 h-14'></input>
-            <button className=' bg-blue-300 h-14 w-12 hover:text-blue-900 justify-center'>
-              <SearchIcon/>
-            </button>
-          </div>
-        </div>
-        <div className='flex ml-64 mt-20'>
-        <div className='w-12 h-14 ml-100 bg-white flex items-center justify-center'>
+          <div className='flex ml-64 mt-20'>
+            <div className='w-14 h-14 bg-white flex items-center justify-center'>
               <LocalPhoneIcon />
             </div>
             <div className='w-40 h-14 bg-blue-300'>
@@ -46,13 +42,14 @@ function Header() {
                 <p className='flex justify-center'>079-558-2457</p>
               </div>
             </div>
+          </div>
         </div>
       </div>
 
       <div>
         <div className='bg-black flex justify-start h-16 items-center'>
           <div className='w-full flex'>
-          <button className='w-40 h-16 text-white hover:text-gray-400 hover:bg-gray-300'>TRANG CHỦ</button>
+            <button className='w-40 h-16 text-white hover:text-gray-400 hover:bg-gray-300'>TRANG CHỦ</button>
             <button className='w-40 h-16 text-white hover:text-gray-400 hover:bg-gray-300'>NƯỚC HOA NAM</button>
             <button className='w-40 h-16 text-white hover:text-gray-400 hover:bg-gray-300'>NƯỚC HOA NỮ</button>
             <button className='w-40 h-16 text-white hover:text-gray-400 hover:bg-gray-300'>LIÊN HỆ</button>

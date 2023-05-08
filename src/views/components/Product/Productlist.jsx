5be6db4ea25c8c905f2ca1a13b3e6832c272e665
Vinh Lê <lephucvinh184@gmail.com>
2023-05-09 01:24:19 +0700

    TE-11:Update ProductList

--- src/views/components/Product/Productlist.jsx
+++ src/views/components/Product/Productlist.jsx
@@ -6,9 +6,9 @@ const Productlist = ({title, productData}) => {
     const newProductData = productData.slice(0,4);
 
   return (
-    <div className='bg-white'>
-      <div className='flex mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8'>
-        <div className='mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8'>
+    <div className='bg-white h-520 mb-12'>
+      <div className='flex mx-auto max-w-2xl sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8'>
+        <div className='mx-auto max-w-2xl sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8'>
           <h2 className='text-2xl font-bold tracking-tight text-gray-900 uppercase overline'>{title}</h2>
           <div className='mt-6 grid grid-cols-4 gap-x-6 gap-y-10  xl:gap-x-8'>
             {

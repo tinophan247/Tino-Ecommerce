4300b5c7e6306f6a0bee17b603ef7f09f75aa6bd
Ona <nguyenduynoa198@gmail.com>
2023-05-10 14:04:15 +0700

    Update UI for Home Page
    Carousel and Footer

--- src/views/pages/Home/index.jsx
+++ src/views/pages/Home/index.jsx
@@ -1,17 +1,14 @@
 import React from 'react';
 import Header from '../../components/Header/Header';
-import Productlist from '../../components/Product/Productlist';
-import { bestSellerProduct, newProduct, saleProduct } from '../../../constant';
-
+import Carousel from '../../components/Carousel';
+import Footer from '../../components/Footer';
 
 const HomePage = () => {
   return (
     <div className='w-full '>
       <Header />
-      <Productlist title='Sản phẩm mới' productData={newProduct}/>
-      <Productlist title='Đang bán chạy' productData={bestSellerProduct}/>
-      <Productlist title='Đang giảm giá' productData={saleProduct}/>
-
+      <Carousel/>
+      <Footer/>
     </div>
   );
 };

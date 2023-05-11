5aad6486595e75d6d91e6057812e11ddf29156a2
Tino Phan <tinophan247@gmail.com>
2023-05-11 23:42:42 +0700

    TE-11 : Design UI for Carousel & Footer

--- src/views/components/Carousel/index.jsx
+++ src/views/components/Carousel/index.jsx
@@ -1,51 +1,46 @@
 import React, { useState, useEffect } from 'react';
+import { slides } from '../../../constant';
+import PrevIcon from '../Icons/PrevIcon';
+import NextIcon from '../Icons/NextIcon';
 
 const Carousel = () => {
-    const slides = [
-        {
-            url: 'https://theme.hstatic.net/1000340570/1000964732/14/slideshow_1.jpg?v=2096',
-        },
-        {
-            url: 'https://theme.hstatic.net/1000340570/1000964732/14/slideshow_2.jpg?v=2096',
-        },
-        {
-            url: 'https://theme.hstatic.net/1000340570/1000964732/14/slideshow_5.jpg?v=2096',
-        },
-    ];
-    const [curr, setCurr] = useState(0);
-    const prev = () =>
-        setCurr((curr) => (curr === 0 ? slides.length - 1 : curr - 1));
-    const next = () =>
-        setCurr((curr) => (curr === slides.length - 1 ? 0 : curr + 1));
-    useEffect(() => {
-        const interval = setInterval(() => {
-            next();
-        }, 3000);
-        return () => clearInterval(interval);
-    }, []);
-    return (
-        <div className="relative m-auto my-4 w-4/5 " >
-            <div
-                className="relative block h-56 bg-cover rounded-lg md:h-96"
-                style={{
-                    backgroundImage: `url(${slides[curr].url})`,
-                    transition: "background-image 0.5s ease-in-out",
-                }}
-            ></div>
-            <button onClick={prev} type="button" className="absolute top-0 left-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
-                <span className="inline-flex items-center justify-center w-8 h-8 rounded-full sm:w-10 sm:h-10 bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-                    <svg aria-hidden="true" className="w-5 h-5 text-white sm:w-6 sm:h-6 dark:text-gray-800" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7"></path></svg>
-                    <span className="sr-only">Previous</span>
-                </span>
-            </button>
-            <button onClick={next} type="button" className="absolute top-0 right-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next>
-                <span className="inline-flex items-center justify-center w-8 h-8 rounded-full sm:w-10 sm:h-10 bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-                    <svg aria-hidden="true" className="w-5 h-5 text-white sm:w-6 sm:h-6 dark:text-gray-800" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7"></path></svg>
-                    <span className="sr-only">Next</span>
-                </span>
-            </button>
-        </div>
-    );
+  const [curr, setCurr] = useState(0);
+  const prev = () => setCurr(curr => (curr === 0 ? slides.length - 1 : curr - 1));
+  const next = () => setCurr(curr => (curr === slides.length - 1 ? 0 : curr + 1));
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      next();
+    }, 3000);
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div className='relative m-auto my-4 w-4/5 '>
+      <div
+        className='relative block h-56 bg-cover rounded-lg md:h-96 transition duration-500 ease-in-out'
+        style={{
+          backgroundImage: `url(${slides[curr].url})`,
+        }}
+      />
+      <button
+        onClick={prev}
+        className='absolute top-0 left-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none'
+      >
+        <span className='inline-flex items-center justify-center w-8 h-8 rounded-full sm:w-10 sm:h-10 bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none'>
+          <PrevIcon/>
+        </span>
+      </button>
+      <button
+        onClick={next}
+        className='absolute top-0 right-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none'
+      >
+        <span className='inline-flex items-center justify-center w-8 h-8 rounded-full sm:w-10 sm:h-10 bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none'>
+          <NextIcon/>
+        </span>
+      </button>
+    </div>
+  );
 };
 
 export default Carousel;
